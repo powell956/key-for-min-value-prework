e8a0cb266cfc5a84afa67c7d28656ccb67145db6
powell956 <powell956@gmail.com>
2018-12-05 16:15:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min = 100
+  smallest = ""
+  name_has.each do |key, value|
+    if
+end
+  
+end
 
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
